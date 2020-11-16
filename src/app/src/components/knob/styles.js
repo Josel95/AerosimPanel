397672a2f77cb08c2b4d471a4952a3b7ac8bec2e
Josel95 <jose.lopezh95@gmail.com>
2020-11-16 12:55:09 -0300

    Changed knob styles

--- src/app/src/components/knob/styles.js
+++ src/app/src/components/knob/styles.js
@@ -22,15 +22,17 @@ export const Container = styled.div`
 
     width: 100px;
     height: 100px;
-    background-color: black;
+    background-color: #cacaca;
+    box-shadow: inset 0px 0px 10px 0px rgba(0,0,0,0.82);
     border-radius: 50%;
 `
 
 export const Knob = styled.div`
     & {
-        width: 80px;
-        height: 80px;
-        background-color: white;
+        width: 70px;
+        height: 70px;
+        background-color: rgb(55,57,59);
+        box-shadow: 0px 0px 11px 1px rgba(0,0,0,0.83);
         border-radius: 50%;
 
         animation-name: ${rotateAnimation};
@@ -44,12 +46,19 @@ export const Knob = styled.div`
         position: relative;
     }
 
+    &:active {
+        box-shadow: none;
+    }
+
     &:after {
         content: '';
-        background-color: red;
-        width: 10px;
-        height: 40px;
+        background-color: white;
+        box-shadow: inset 0px 0px 11px 1px rgba(0,0,0,0.83);
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
         position: absolute;
-        left: calc(50% - 5px);
+        top: 5px;
+        left: calc(50% - 7.5px);
     }
 ` 
